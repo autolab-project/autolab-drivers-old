a548918d2e679391f8e00960d475788d0f83f011
bruno066 <bruno.garbin.etu@gmail.com>
2020-09-16 18:53:40 +0200

    Initial commit of the drivers

--- init_git.py
+++ init_git.py
@@ -2,6 +2,8 @@
 
 import os
 import shutil
+import configparser
+from datetime import date
 
 def import_drivers_from_autolab_git(path_to_drivers,path_to_new_git):
     
@@ -17,14 +19,16 @@ def import_drivers_from_autolab_git(path_to_drivers,path_to_new_git):
         shutil.rmtree(driver_path_to_remove)
     
     # Copying all drivers from autolab into path_to_new_git with new structure (driver_MODEL/v1.0.0/driver_MODEL.py)
-    drivers_dir_list_to_copy = [name for name in os.listdir(path_to_drivers) if name not in ['init_git.py','__init__.py','.git','__pycache__']]
-    for driver_dir in drivers_dir_list_to_copy:
+    dir_list_to_copy = [name for name in os.listdir(path_to_drivers) if name not in ['init_git.py','__init__.py','.git','__pycache__']]
+    for driver_dir in dir_list_to_copy:
         # Create path to driver to copy
         driver_path_to_copy = os.path.join(path_to_drivers,driver_dir)
         
-        # Create a directory appropriate directory structure using the driver name
+        # Create an appropriate directory structure using the driver name
         new_driver_dir = os.path.join(path_to_new_git,driver_dir)
-        if driver_dir == 'More':
+        # Do we need versionning for the directory?
+        list_dir_without_versioning = ['More']
+        if driver_dir in list_dir_without_versioning:
             new_sub_driver_dir = new_driver_dir
         else:
             os.mkdir(new_driver_dir)  # create dir only if v1.0.0 has to be created
@@ -32,3 +36,15 @@ def import_drivers_from_autolab_git(path_to_drivers,path_to_new_git):
         
         # Copy driver directory to git
         shutil.copytree(driver_path_to_copy,new_sub_driver_dir)
+        
+        # Write release_notes.ini
+        if driver_dir not in list_dir_without_versioning:
+            config = configparser.ConfigParser()
+            config.add_section('release_notes')
+            config['release_notes']['version'] = '1.0.0'
+            config['release_notes']['date']    = date.today().strftime("%d-%b-%Y")
+            config['release_notes']['notes']   = "First push from autolab's previous version"
+            
+            path_release_notes = os.path.join(new_sub_driver_dir,'release_notes')
+            with open(path_release_notes, 'w') as configfile:    # save
+                config.write(configfile)
